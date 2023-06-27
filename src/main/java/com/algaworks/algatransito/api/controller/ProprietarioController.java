0f6326929dba247b5b12f22c26d85bf6bff81983
Dyan Carvalho <soudyancarvalho@gmail.com>
2023-06-27 19:44:19 -0300

    Implementando uma consulta com JPQL

--- src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -1,6 +1,9 @@
 package com.algaworks.algatransito.api.controller;
 
 import com.algaworks.algatransito.domain.model.Proprietario;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,21 +13,13 @@ import java.util.List;
 @RestController
 public class ProprietarioController {
 
+    @PersistenceContext
+    public EntityManager manager;
+
     @GetMapping("/proprietarios")
     public List<Proprietario> listar(){
-        var proprietario1 = new Proprietario();
-        proprietario1.setId(1L);
-        proprietario1.setNome("Joao");
-        proprietario1.setEmail("71 98879-9334");
-        proprietario1.setTelefone("joao@algaworks.com");
-
-        var proprietario2 = new Proprietario();
-        proprietario2.setId(2L);
-        proprietario2.setNome("Maria");
-        proprietario2.setEmail("71 98879-0000");
-        proprietario2.setTelefone("maria@algaworks.com");
-
-        return Arrays.asList(proprietario1, proprietario2);
+        TypedQuery<Proprietario> query = manager.createQuery("from Proprietario", Proprietario.class);
+        return query.getResultList();
     }
 
 }

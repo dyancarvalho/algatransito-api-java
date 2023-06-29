55977a1f339f1c12a2cf36f1310c60c5141354cb
Dyan Carvalho <soudyancarvalho@gmail.com>
2023-06-29 16:23:34 -0300

    Validando entrada de dados com Jakarta Bean Validation

--- src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
+++ src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
@@ -1,6 +1,9 @@
 package com.algaworks.algatransito.domain.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,9 +20,17 @@ public class Proprietario {
     @EqualsAndHashCode.Include
     private Long id;
 
+    @NotBlank
+    @Size(max = 60)
     private String nome;
+
+    @NotBlank
+    @Size(max = 255)
+    @Email
     private String email;
 
+    @NotBlank
+    @Size(max = 20)
     @Column(name = "fone")
     private String telefone;
 
